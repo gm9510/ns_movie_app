4b3f7eba035ed49a51ba9918611e7a96cfc0f3db
Gustavo Quijote <gm9510@gmail.com>
2021-02-24 08:41:48 -0500

    Variables del objeto observable definidas y la función http request añadida y conectando con la api de peliculas

--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,9 +1,30 @@
 const core = require("@nativescript/core");
+const { apiUrl, apiKey } = require("./../config");
+
 
 function HomeViewModel() {
     const viewModel = core.fromObject({
         /* Add your view model properties here */
+        Title: "Pelicula",
+        Year: "2000",
+        Director: "Alguien",
+        Runtime: "90 min",
+        Poster: "Link",
     });
+
+    viewModel.requestMovie = async() => {
+        const URL = `${apiUrl}?t=avatar&apikey=${apiKey}`;
+        const response = await fetch(URL);
+        const { Title, Year, Director, Runtime, Poster } = await response.json();
+
+        console.log({Title});
+
+        viewModel.set("Title",Title);
+        viewModel.set("Year",Year);
+        viewModel.set("Director",Director);
+        viewModel.set("Runtime",Runtime);
+        viewModel.set("Poster",Poster);
+    };
 
     return viewModel;
 }
