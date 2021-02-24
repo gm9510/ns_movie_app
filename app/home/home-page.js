40b6fa0355854da9fbaf69e94b0a7531d66c2010
Gustavo Quijote <gm9510@gmail.com>
2021-02-24 17:12:20 -0500

    Carga la siguiente película en la lista cuando se realizá un swipe a la izquierda y la derecha

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -5,8 +5,40 @@ var home = new HomeViewModel();
 function onNavigatingTo(args) {
     const page = args.object;
     console.log("Page Loaded");
-    home.requestMovie();
+    home.set("isLoading", true);
+    home.requestMovie().then( () =>{
+        home.set("isLoading", false);
+    });
     page.bindingContext = home;
 }
 
 exports.onNavigatingTo = onNavigatingTo;
+
+function onSwipe(args) {
+    let index = home.get("index");
+    switch (args.direction){
+        case 1:
+            //home.rightSwipe();
+            home.set("index",index-1);
+            home.set("isLoading", true);
+            home.requestMovie().then( () =>{
+                home.set("isLoading", false);
+            });
+            console.log("right");
+            break;
+        case 2:
+            //home.leftSwipe();
+            home.set("index",index+1);
+            home.set("isLoading", true);
+            home.requestMovie().then( () =>{
+                home.set("isLoading", false);
+            });
+            console.log("left");
+            break;
+    }
+}
+
+exports.onSwipe = onSwipe;
+
+
+
